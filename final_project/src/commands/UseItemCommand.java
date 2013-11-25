17431b99fec1faeae88fdf1f2b208cd3ff1a18f4
Kyle Criddle <criddlek@email.arizona.edu>
2013-11-24 21:15:14 -0700

    added new use item comand

---
+++ final_project/src/commands/UseItemCommand.java
@@ -0,0 +1,22 @@
+package commands;
+
+import shared.Game;
+import shared.Item;
+
+@SuppressWarnings("serial")
+public class UseItemCommand extends GameCommand {
+
+	private Item item;
+	
+	public UseItemCommand(int[] source, int[] dest, Item item) {
+		this.source = source;
+		this.dest = dest;
+		this.item = item;
+	}
+	
+	public void execute(Game g) {
+		//g.useItem(source, dest, item);
+	}
+	
+	
+}
