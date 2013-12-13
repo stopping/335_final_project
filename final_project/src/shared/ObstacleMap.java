6ffd09e60764eda7876de401c94d140eae069f9c
Nathaniel Lin <nylin@boxer.cs.arizona.edu>
2013-12-12 17:02:07 -0700

    Beef up the mapBehavior by having the maps set the board

--- final_project/src/shared/ObstacleMap.java
+++ final_project/src/shared/ObstacleMap.java
@@ -1,9 +1,14 @@
 package shared;
 
+import java.util.ArrayList;
+
+import shared.GameSquare.Terrain;
+import unit.Unit;
+
 @SuppressWarnings("serial")
 public class ObstacleMap implements MapBehavior {
 	
-	private char[][] board = {
+	private char[][] charBoard = {
 			{'X','X','X','X','X',' ',' ',' ',' ',' ',' ',' '},
 			{'X','R','R','X',' ','X',' ',' ',' ','X','X','X'},
 			{'X','R','R','X',' ',' ','X',' ','X',' ',' ',' '},
@@ -17,13 +22,29 @@ public class ObstacleMap implements MapBehavior {
 			{' ','X',' ',' ','X',' ',' ',' ','X','B','B','B'},
 			{'X',' ',' ',' ',' ','X',' ',' ',' ','X','X','X'}};
 	
+	
+	private GameSquare[][] board;
 
+	
 	public ObstacleMap() {
-		
+		board = new GameSquare[charBoard.length][charBoard[0].length];
 	}
 	
-	
-	public char[][] getMap() {
+	@Override
+	public GameSquare[][] getMap() {
 		return board;
+	}
+	
+	public void setMap(ArrayList<Unit> redUnits, ArrayList<Unit> blueUnits) {
+		int redCount = 0, blueCount = 0;
+		
+		for (int r = 0; r < charBoard.length; r++) {
+			for (int c = 0; c < charBoard[r].length; c++) {
+				board[r][c] = new GameSquare( Terrain.Grass, r, c );
+				if(charBoard[r][c] == 'X') board[r][c].setOccupant(new WallObstacle());
+				if(charBoard[r][c] == 'R') board[r][c].setOccupant(redUnits.get(redCount++));
+				if(charBoard[r][c] == 'B') board[r][c].setOccupant(blueUnits.get(blueCount++));
+			}
+		}
 	}
 }
